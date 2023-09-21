41e3216a5d59f91b232ee3b2ffd0fafcd70ae5a5
varchar213 <jmcs7eg1@gmail.com>
2023-09-21 17:03:30 +0900

    fork로 테스트

---
+++ src/main/java/org/kosta/myproject/ForkTest3BootApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkTest3BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkTest3BootApplication.class, args);
+	}
+
+}
